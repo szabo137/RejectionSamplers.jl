b45af98a89f0c2829ab06902ac4796db61272131
Uwe Hernandez Acosta <u.hernandez@hzdr.de>
2026-08-10 10:40:21 +0200

    finalized benchmarks for batch eval

---
+++ benchmarks/02_JuliaCon2026/benchmarks/batch/single_stage_batchless.jl
@@ -0,0 +1,56 @@
+bench_name = "single_stage_batchless"
+@info "Adding benchmark: $bench_name"
+nevent_vec = 2 .^ (5:6)
+@info "used nevents: $nevent_vec"
+batch_size_vec = 2 .^ (5:6)
+@info "used batch sizes: $batch_size_vec"
+
+group = addgroup!(SUITE, bench_name)
+for out_type in DTYPES
+
+    local _group = addgroup!(group, "$out_type")
+
+    for dim in DIMS
+
+        local __group = addgroup!(_group, dim)
+
+        in_type = SVector{dim, out_type}
+
+        target, proposal, max_val = generate_setup(out_type, dim)
+
+        SAMPLER = RejectionSampler(target, proposal, max_val; backend = BACKEND, in_type = in_type, out_type = out_type)
+
+        for N in nevent_vec
+            for batch_size in batch_size_vec
+                @info "Adding benchmark problem: dtype=$out_type, dim=$dim, Neve=$N, Nbatch=$batch_size"
+
+                __group[batch_size][N] = @benchmarkable @sb(
+                    begin
+                        sample_kernel(
+                            target,
+                            proposal,
+                            max_val,
+                            output;
+                            ndrange = $batch_size
+                        )
+                        output.level .= zero(UInt32)
+                    end
+                ) setup = begin
+                    # Allocate output buffers
+                    output = OutBuffer(
+                        get_backend($SAMPLER),
+                        input_type($SAMPLER),
+                        output_type($SAMPLER),
+                        $N + $batch_size # one additional batch for safety
+                    )
+
+                    sample_kernel = RejectionSamplers.sample_batchless_kernel($BACKEND, 32)
+                    backend = get_backend($SAMPLER)
+                    target = target_distribution($SAMPLER)
+                    proposal = proposal_distribution($SAMPLER)
+                    max_val = maximum_value($SAMPLER)
+                end
+            end
+        end
+    end
+end
